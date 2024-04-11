ea218e1a2f3df018772bd9c013c61bddc50dd301
Andy Leon <andyleon000@gmail.com>
2024-04-11 00:59:14 -0400

    updated sets

--- modules/ptcg_sets_data.py
+++ modules/ptcg_sets_data.py
@@ -156,5 +156,7 @@ ptcg_sets = [
 	{'id': 'sv2', 'name': 'Paldea Evolved', 'ptcgoCode': 'PAL'},
 	{'id': 'sv3', 'name': 'Obsidian Flames', 'ptcgoCode': 'OBF'},
 	{'id': 'sv3pt5', 'name': '151', 'ptcgoCode': 'MEW'},
-    {'id': 'sv4', 'name': 'Paradox Rift', 'ptcgoCode': 'PAR'}
+    {'id': 'sv4', 'name': 'Paradox Rift', 'ptcgoCode': 'PAR'},
+    {'id': 'sv4pt5', 'name': 'Paldean Fates', 'ptcgoCode': 'PAF'},
+    {'id': 'sv5', 'name': 'Temporal Forces', 'ptcgoCode': 'TEF'}
 ]
